d0941db5d5cfac774bfd5cfb0d12af475c7e93ea
gmd1apeterbos <97040824@st.deltion.nl>
2017-12-12 13:11:26 +0100

    More stuff

---
+++ Card game prototype/Assets/Scripts/MonsterCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCard : MonoBehaviour {
+    private bool zoomedIn;
+    public Vector2 zoomedCardPos;
+    private GameObject temp;
+	// Use this for initialization
+	void Start () {
+        zoomedCardPos.x = 7.5F;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!zoomedIn)
+            {
+                temp = Instantiate(gameObject, zoomedCardPos, Quaternion.identity);
+                temp.transform.localScale = new Vector3(0.9F, 0.9F, 1);
+                temp.GetComponent<MonsterCard>().enabled = false;
+                temp.GetComponent<BoxCollider2D>().enabled = false;
+               
+                zoomedIn = true;
+            }
+            else if (zoomedIn)
+            {
+                Destroy(temp);
+                zoomedIn = false;
+            }
+        }
+    }
+}
