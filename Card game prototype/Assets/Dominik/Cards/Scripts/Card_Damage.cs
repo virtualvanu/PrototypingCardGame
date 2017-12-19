1bfa6a0473f390f0e7fef13ee54933da43fcb2c8
Dominik Beens <dominik.beens@hotmail.nl>
2017-12-19 09:37:44 +0100

    more card setup

--- Card game prototype/Assets/Dominik/Cards/Scripts/Card_Damage.cs
+++ Card game prototype/Assets/Dominik/Cards/Scripts/Card_Damage.cs
@@ -16,11 +16,52 @@ public class Card_Damage : Card
         myHolder.CreateAttribute(0, damage);
     }
 
-    public override void Use()
+    public override void Use(CardHolder myHolder)
     {
-        base.Use();
+        base.Use(myHolder);
 
-        // damage the enemy
+        switch (myHolder.side)
+        {
+            case CardHolder.Side.Enemy:
+
+                switch (target)
+                {
+                    case Target.Ally:
+
+                        FightManager.instance.enemy.currentHealth -= damage;
+                        break;
+                    case Target.Enemy:
+
+                        FightManager.instance.player.currentHealth -= damage;
+                        break;
+                    case Target.Both:
+
+                        FightManager.instance.enemy.currentHealth -= damage;
+                        FightManager.instance.player.currentHealth -= damage;
+                        break;
+                }
+                break;
+            case CardHolder.Side.Player:
+
+                switch (target)
+                {
+                    case Target.Ally:
+
+                        FightManager.instance.player.currentHealth -= damage;
+                        break;
+                    case Target.Enemy:
+
+                        FightManager.instance.enemy.currentHealth -= damage;
+                        break;
+                    case Target.Both:
+
+                        FightManager.instance.player.currentHealth -= damage;
+                        FightManager.instance.enemy.currentHealth -= damage;
+                        break;
+                }
+                break;
+        }
+
         Debug.Log("Damage card did " + damage + " damage");
     }
 }
