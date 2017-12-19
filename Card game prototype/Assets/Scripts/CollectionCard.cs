c191b5e0c6241afbb70212a789113e5e8b8f7d48
gmd1apeterbos <97040824@st.deltion.nl>
2017-12-19 08:56:40 +0100

    Moest nog even committen

---
+++ Card game prototype/Assets/Scripts/CollectionCard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CollectionCard : MonoBehaviour {
+    public Image mySprite;
+    //private Card thisCard;
+    public DeckBuilding deckEditor;
+	// Use this for initialization
+	void Start () {
+        //If scene = collection scene of iets dergelijks
+        //thisCard = GetComponent<Card>();
+        mySprite = GetComponent<Image>();
+        deckEditor = GameObject.FindGameObjectWithTag("DE").GetComponent<DeckBuilding>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        if (Input.GetMouseButtonDown(0) && deckEditor.isEditing)
+        {
+            deckEditor.playerDeck.Add(gameObject);
+            deckEditor.ShowDeckCards();
+        }
+       
+    }
+}
---
+++ Card game prototype/Assets/Scripts/DeckBuilding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DeckBuilding : MonoBehaviour {
+    public List<GameObject> playerDeck = new List<GameObject>();
+    private int instantiatedCards;
+    private GameObject deckContent;
+    public bool isEditing;
+	// Use this for initialization
+	void Start () {
+        deckContent = GameObject.FindGameObjectWithTag("DeckContent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartEditing()
+    {
+        if (!isEditing)
+        {
+            isEditing = true;
+        }
+        else if (isEditing)
+        {
+            isEditing = false;
+        }
+    }
+
+    public void ShowDeckCards()
+    {
+        foreach(GameObject g in playerDeck)
+        {
+            if(instantiatedCards < playerDeck.Count)
+            {
+                GameObject q = Instantiate(g, Vector3.zero, Quaternion.identity);
+                q.transform.SetParent(deckContent.transform);
+                q.GetComponent<RectTransform>().SetAsFirstSibling();
+                instantiatedCards++;
+            }
+        }
+    }
+}
