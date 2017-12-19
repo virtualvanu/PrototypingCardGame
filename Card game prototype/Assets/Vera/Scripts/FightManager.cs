270a3abdb837c289a45df31e88f96f2505537688
VeraGerritseDeltion <arev_97@live.nl>
2017-12-19 09:16:50 +0100

    chars en manager

---
+++ Card game prototype/Assets/Vera/Scripts/FightManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour {
+    public static FightManager instance;
+    public Character player;
+    public Character enemy;
+    public List<Card> playerDeck;
+    public List<Card> enemyDeck;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public static void SetDeck(List<Card> cards)
+    {
+
+    }
+}
