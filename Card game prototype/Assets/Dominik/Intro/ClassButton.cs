e49c11bf5d852dbd2f6740370af386faa3a70e86
Dominik Beens <dominik.beens@hotmail.nl>
2017-12-21 11:44:08 +0100

    work on intro

---
+++ Card game prototype/Assets/Dominik/Intro/ClassButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassButton : MonoBehaviour
+{
+
+    public Character pickableCharacter;
+
+    [Header("Lerping")]
+    public bool canLerp;
+    public Transform lerpToTarget;
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private void Update()
+    {
+        if (canLerp)
+        {
+            Lerp();
+        }
+    }
+
+    public void Lerp()
+    {
+        transform.position = Vector3.Lerp(transform.position, lerpToTarget.position, (Time.deltaTime * moveSpeed));
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.zero), (Time.deltaTime * rotateSpeed));
+    }
+}
---
+++ Card game prototype/Assets/Dominik/Intro/IntroManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+
+    public static bool hasCharacter;
+
+    public GameObject titlePanel;
+
+    public GameObject characterCreationPanel;
+    public Transform classButtons;
+
+    public float buttonLerpDelay;
+
+    public void PlayButton()
+    {
+        if (hasCharacter)
+        {
+            // continue to level select scene
+        }
+        else
+        {
+            // create a character
+            titlePanel.SetActive(false);
+            characterCreationPanel.SetActive(true);
+            StartCoroutine(ReadyClassButtons());
+        }
+    }
+
+    public IEnumerator ReadyClassButtons()
+    {
+        foreach (Transform child in classButtons)
+        {
+            child.gameObject.SetActive(true);
+            child.GetComponent<ClassButton>().canLerp = true;
+
+            yield return new WaitForSeconds(buttonLerpDelay);
+        }
+    }
+}
